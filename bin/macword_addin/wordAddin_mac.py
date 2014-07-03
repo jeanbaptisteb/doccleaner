85b4c93be033734e3980afbc5c134da8ce0ad809
jbber <jbber@users.noreply.github.com>
2014-07-03 23:19:35 +0200

    Delete wordAddin_mac.py
    not finished and not useful for the moment

--- bin/macword_addin/wordAddin_mac.py
+++
@@ -1,8 +0,0 @@
-tell application "Microsoft Word"
-   save active document
-   set originDoc to name of active document
-   set tmp_dir to path to temporary items
-   tell active document
-	
-   end tell
-end tell
